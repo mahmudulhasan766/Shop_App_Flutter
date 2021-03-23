b9112b85756a3e9f6c1abb2c161745857e2af85d
HaSaN <mahamudul951@gmail.com>
2021-03-24 00:12:29 +0600

    sdsad

--- lib/sdf.dart
+++ lib/sdf.dart
@@ -4,6 +4,9 @@ class sdsad extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("SdSad"),
+      ),
       body: Container(
         color: Colors.greenAccent,
       ),
