f450ef6863e4c2707d55c0d301f14e24bad0a765
HaSaN <mahamudul951@gmail.com>
2021-03-26 23:22:13 +0600

    ChatSystem

---
+++ lib/chat_system.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatSystem extends StatefulWidget {
+  @override
+  _ChatSystemState createState() => _ChatSystemState();
+}
+
+class _ChatSystemState extends State<ChatSystem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+    );
+  }
+}
